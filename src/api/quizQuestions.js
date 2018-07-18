0b2f5c45fa40bd8a0a37010fdcd345e1f8d42e80
Timothy Swengros <timswengros@Timothys-MacBook-Pro.local>
2018-07-17 21:01:43 -0400

    added questions

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,90 +1,86 @@
 var quizQuestions = [
     {
-        question: "What is your spirit animal ? ",
+        question: "How does The Incredible Hulk Coaster launch the trains every minute and a half?",
         answers: [
             {
-                type: "Casual Tourist",
-                content: "Wolf"
-                
-            },
-    
-   
-   
-            {
-                type: "Interested Tourist",
-                content: "Lion"
+                type: "a casual park-goer!",
+                content: "Wound Up Catch Car" 
             },
             {
-                type: "Incredible Tourist!",
-                content: "Dragon"
+                type: "an interested park-goer!",
+                content: "Electromagnets"
+            },
+            {
+                type: "a park and attraction enthusiast!",
+                content: "Rows of airplane tires"
             }
         ]
     },
     {
-        question: "What do you value most ? ",
+        question: "Doctor Doom's Fearfall utilizes what technology to launch the ride vehicles? ",
         answers: [
             {
-                type: "Stark",
-                content: "Loyalty"
+                type: "an interested park-goer!",
+                content: "Hydraulics"
             },
             {
-                type: "Lannister",
-                content: "Money"
+                type: "a park and attraction enthusiast!",
+                content: "Compressed air"
             },
             {
-                type: "Targaryen",
-                content: "Power"
+                type: "a casual park-goer!",
+                content: "Giant Rubber Bands" 
             }
         ]
     },
     {
-        question: "Who's your favorite family member ?",
+        question: "5 of the 7 Islands inside Islands of Adventure have their begginnings in what?",
         answers: [
             {
-                type: "Stark",
-                content: "My Parent"
+                type: "a casual park-goer!",
+                content: "The Opposite of what Disney has done" 
             },
             {
-                type: "Lannister",
-                content: "My Sibling"
+                type: "a park and attraction enthusiast!",
+                content: "Books"
             },
             {
-                type: "Targaryen",
-                content: "My Grandparent"
+                type: "an interested park-goer!",
+                content: "The Latest in Pop Culture"
             }
         ]
     },
     {
-        question: "Choose an element",
+        question: "Islands of Adventure opened in 1999 as The Most Technologically Advanced Theme Park in the World.  From Conception to Opening Day, how long did the park take to build?",
         answers: [
             {
-                type: "Stark",
-                content: "Water"
+                type: "a casual park-goer!",
+                content: "3 years" 
             },
             {
-                type: "Lannister",
-                content: "Air"
+                type: "a park and attraction enthusiast!",
+                content: "10 years"
             },
             {
-                type: "Targaryen",
-                content: "Fire"
+                type: "an interested park-goer!",
+                content: "7 years"
             }
         ]
     },
     {
-        question: " Which Castle would you live in ? ",
+        question: "The parks icon, the lighthouse, is based off of what Wonder of the Ancient World?",
         answers: [
             {
-                type: "Stark",
-                content: "Winterfell"
+                type: "an interested park-goer!",
+                content: "The Tower of Hercules"
             },
             {
-                type: "Lannister",
-                content: "Casterly Rock"
+                type: "a park and attraction enthusiast!",
+                content: "The Great Lighthouse at Alexandria"
             },
             {
-                type: "Targaryen",
-                content: "DragonStone"
+                type: "a casual park-goer!",
+                content: "Bell-Rock Lighthouse" 
             }
         ]
     }
