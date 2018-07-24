27e8258389b4ee851527a9f8aed284608576e877
Timothy Swengros <timswengros@Timothys-MacBook-Pro.local>
2018-07-24 14:01:41 -0400

    attempted login

---
+++ config/passport.js
@@ -0,0 +1,46 @@
+
+const passport = require('passport'),
+FacebookStrategy = require('passport-facebook').Strategy,
+keys = require('./keys'),
+db = require('../models')
+
+passport.serializeUser((user, done) => { done(null, user.id) })
+passport.deserializeUser((id, done) => {
+    db.User.findById(id).then( user => { done(null, user) })
+})
+
+passport.use(new FacebookStrategy({
+    // options for the strategy
+    clientID: keys.facebook.clientID,
+    clientSecret: keys.facebook.clientSecret,
+    callbackURL: "/auth/facebook/redirect",
+    proxy: true
+}, (accessToken, refreshToken, profile, done) => {
+    
+    
+    // passport callback function
+    // Check if user already exitsts in our db
+    db.User.findOne({
+            where: {fbId: profile.id},   
+    })
+    .then(User => {
+        if(User){
+            // already have the user
+            done(null, User)
+        }
+        else {
+            // if User doesnt exit we will create a new one
+            db.User.create({ name: profile.displayName, fbId: profile.id })
+            .then(User => {
+                db.DinnerPreference.create({UserId: User.id})
+                .then(DinnerPreference => {
+                    db.UserPreference.create({UserId: User.id})
+                    .then(UserPreference => {
+                        done(null, User)
+                    })
+                })
+            })
+        }
+    })
+
+}))
